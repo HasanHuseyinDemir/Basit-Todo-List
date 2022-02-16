d036fac511816b11de4964bec816789776d866ca
trojan565 <majorexo565@gmail.com>
2022-02-16 17:54:48 +0300

    "Add new item" function added

---
+++ script.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("form");
+const input = document.querySelector("#txtTaskName");
+const btnDeleteAll = document.querySelector("#btnDeleteAll");
+const taskList = document.querySelector("#task-list");
+
+eventlisteners(); // uygulama başlayınca aktif olur
+
+function eventlisteners(){
+    form.addEventListener("submit",addNewItem);
+
+}
+
+function addNewItem(e){ // yeni item eklemeye yarar
+    if(input.value == ""){  
+        alert("Add New Item"); //burada boş bırakılan input değerini uyarı olarak verir 
+    }
+
+    const listeElemani = document.createElement("li");
+    listeElemani.className="list-group-item list-group-item-secondary";
+    listeElemani.appendChild(document.createTextNode(input.value)); // input içine yazılan veriyi liste elemanını içine text olarak aktarmış olur.
+
+    const aElementi = document.createElement("a"); 
+    aElementi.classList="delete-item float-right"; //a nın class ı
+    aElementi.setAttribute("href","#"); 
+    aElementi.innerHTML= "<i class='fas fa-times'></i>";
+    console.log(listeElemani);
+
+    listeElemani.appendChild(aElementi);// burada liste elemanının içine a elementini yerleştirmiş olduk.
+    taskList.appendChild(listeElemani); // burada ise liste elemanını listeye eklemiş olduk :) 
+    e.preventDefault(); // formun sürekli submit olmasını engeller
+}
